f4c72346cf11f1b962476bb58cb914e0df5284a5
Claudio Tallè <klo@macbook-capitan.local>
2016-08-20 18:39:27 +0200

    Initial Commit

---
+++ SecondSwiftTest/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  SecondSwiftTest
+//
+//  Created by Claudio Tallè on 18/08/16.
+//  Copyright © 2016 Claudio Tallè. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    @IBOutlet weak var nameTextField: NSTextField!
+    @IBOutlet weak var welcomeLabel: NSTextField!
+    @IBAction func handleWelcome(sender: AnyObject) {
+        welcomeLabel.stringValue = "Hello \(nameTextField.stringValue)!"
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        var 💏At:String
+         💏At = "Pippo"
+        
+        var 👉io:Int
+        👉io = 100
+        
+        
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
